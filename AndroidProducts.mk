8607388245527ef94b8eeb90b1b2ed8aa5ed347a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-03-22 14:52:53 +0100

    TECNO-KM7k: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: tecno Model: TECNO KM7k Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KM7k.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KM7k-user \
+    omni_TECNO-KM7k-userdebug \
+    omni_TECNO-KM7k-eng
